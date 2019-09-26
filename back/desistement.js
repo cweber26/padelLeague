be10278b83ff27711260f1b3e5ee9307d31f1cbd
Cédric Weber <cedric.weber@decathlon.com>
2019-09-26 20:07:14 +0200

    improve desistement mails

--- back/desistement.js
+++ back/desistement.js
@@ -10,25 +10,24 @@ function checkIfDesistement(parameter, playersInTheMatchMailBefore) {
 
 function actionsToDoIfDesistement(desisteurMail) {
     if (!isMatchCancel()) {
+        var oldPlayer = getPlayerWithMail(desisteurMail);
         if (parametersMap.get("numberPlayerInMatch") == parametersMap.get("numberPlayerMatch") && parametersMap.get("numberPlayerInWaitingList") > 0) {
             //the match was full and a least a player was in waiting list
-            var player = getNewPlayerInCompo();
+            var newPlayer = getNewPlayerInCompo();
             // we get the new player
             if (isParameterBlank("mailSendingReminder")) {
-                sendMail(player.mail, "Tu es sélectionné pour le match " + matchDayGapInFrench(true) + " en raison d'un désitement", getBodyMailReminder(player, true));
+                sendMail(newPlayer.mail, "Tu es sélectionné pour le match " + matchDayGapInFrench(true) + " en raison d'un désitement", getBodyMailReminder(newPlayer, true));
             } else if (isParameterBlank("mailSendingConfirmation")) {
-                sendRemindMailForAPlayer(player, true);
+                sendRemindMailForAPlayer(newPlayer, true);
             } else {
-                sendConfirmMailForAPlayer(player, true);
+                sendConfirmMailForAPlayer(newPlayer, true);
             }
-            var fullName = player.fullName;
-            sendMailForAdmin("Alerte : l'effectif de l'équipe a changé",
-                "<h4>" + desisteurMail + " s'est désité pour le match " + matchDayGapInFrench(true) + "</h4><h4>" + fullName + " est dispo pour le remplacer</h4>");
+            sendMailForAdmin("Remplacement : " + oldPlayer.fullName + " s'est désité pour le match " + matchDayGapInFrench(true),
+                "<h4>" + oldPlayer.fullName + " s'est désité pour le match " + matchDayGapInFrench(true) + "</h4><h4>" + newPlayer.fullName + " est dispo pour le remplacer</h4>");
         } else {
             var nbAvailableSlotsDesistement = (parametersMap.get("numberAvailableSlotInMatch")+1);
-            sendMailForAdmin("Alerte : il manque " + nbAvailableSlotsDesistement + " joueur(s) pour le match " + matchDayGapInFrench(true),
-                "<h4>" + desisteurMail + " s'est désité pour le match " + matchDayGapInFrench(true) + "</h4><h4>Il manque " + nbAvailableSlotsDesistement + " joueur(s)</h4>");
+            sendMailForAdmin("Alerte : " + oldPlayer.fullName + " s'est désité pour le match " + matchDayGapInFrench(true) + " il manque " + nbAvailableSlotsDesistement + " joueur(s) pour le match", "<h4>" + oldPlayer.fullName + " s'est désité pour le match " + matchDayGapInFrench(true) + "</h4><h4>Il manque " + nbAvailableSlotsDesistement + " joueur(s)</h4>");
         }
+        updateCalendarEvent();
     }
-    updateCalendarEvent();
 }
